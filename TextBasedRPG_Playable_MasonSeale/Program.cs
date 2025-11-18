dee43e3afe3adf40910252768ce877f7aeb91e62
Bosssmann810 <sealered@gmail.com>
2025-11-18 15:05:09 -0400

    working on it still

--- TextBasedRPG_Playable_MasonSeale/Program.cs
+++ TextBasedRPG_Playable_MasonSeale/Program.cs
@@ -9,17 +9,23 @@ namespace TextBasedRPG_MasonSeale
 {
     internal class Program
     {
+        static List<(int, int)> spots;
         static bool enemystuck;
         static bool playerstuck = false;
         static bool going = true;
         static (int,int) playerpos = (10,5);
         static (int,int) Ppreviouspos = ( 2, 2 );
+        static int otherenmyhp = 20;
+        static (int, int) otherenemypos = (20,10);
+        static (int, int) Opreviouspos = (20, 10);
         static (int,int) enemypos = ( 12, 7 );
         static (int, int) Epreviouspos = (2, 2);
         static Random enemyrng = new Random();
-        static int playerhp = 10;
+        static int playerhp = 30;
         static int enemyhp = 10;
-
+        static bool tired = false;
+        static int verticlebounds = 0;
+        static int horziontalbound;
 
         static void Main(string[] args)
         {
@@ -28,15 +34,16 @@ namespace TextBasedRPG_MasonSeale
             while (going == true)
             {
                
-                endcheck();
+                
                 Console.SetCursorPosition(0, 0);
                 Console.WriteLine();
                 hud();
                 DisplayMap();
+                  
                 enemymovement();
-                playermovment();
-
-
+                otherenemymovement();
+                playermovment();  
+                endcheck();
             }
 
 
@@ -49,8 +56,8 @@ namespace TextBasedRPG_MasonSeale
 
             string path = "map.txt";
             string[] map = File.ReadAllLines(path);
-
-
+            horziontalbound = map[0].Length;
+            verticlebounds = 0;
 
             if (x == 1)
             {
@@ -67,24 +74,31 @@ namespace TextBasedRPG_MasonSeale
                     {
                         if(j == '+')
                         {
+                            Console.BackgroundColor = ConsoleColor.Black;
                             Console.ForegroundColor = ConsoleColor.DarkYellow;
                         }
                         if(j == '~')
                         {
+                            
+                            Console.BackgroundColor = ConsoleColor.DarkBlue;
                             Console.ForegroundColor = ConsoleColor.Blue;
                         }
                         if(j == '*')
                         {
+                            Console.BackgroundColor = ConsoleColor.Black;
                             Console.ForegroundColor = ConsoleColor.Yellow;
                         }
                         if(j == '^')
                         {
+                            Console.BackgroundColor = ConsoleColor.Black;
                             Console.ForegroundColor = ConsoleColor.DarkGray;
                         }
                         if(j == '`')
                         {
+                            Console.BackgroundColor = ConsoleColor.Black;
                             Console.ForegroundColor = ConsoleColor.DarkGreen;
                         }
+                        verticlebounds += 1;
                         Console.Write(j);
                     }
 
@@ -96,43 +110,36 @@ namespace TextBasedRPG_MasonSeale
                 Console.Write("└");
                 Console.Write(current);
                 Console.Write("┘");
+
+                Console.SetCursorPosition(enemypos.Item1, enemypos.Item2);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("i");
+                Console.SetCursorPosition(otherenemypos.Item1, otherenemypos.Item2);
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write("i");
+                Console.SetCursorPosition(playerpos.Item1, playerpos.Item2);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("i");
+                if (enemyhp <= 0)
+                {
+                    Console.SetCursorPosition(enemypos.Item1, enemypos.Item2);
+                    Console.Write(" ");
+                }
+                if (otherenmyhp <= 0)
+                {
+                    Console.SetCursorPosition(otherenemypos.Item1, otherenemypos.Item2);
+                    Console.Write(" ");
+                }
             }
-        }
-
-        static void legend()
-        {
-
-            Console.WriteLine("┌────────────┐");
-            Console.WriteLine("│Legend:     │");
-            Console.Write("│");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("' = grass   ");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("│");
-            Console.WriteLine();
-            Console.Write("│");
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.Write("~ = water   ");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("│");
-            Console.WriteLine();
-            Console.Write("│");
-            Console.ForegroundColor = ConsoleColor.DarkGray;
-            Console.Write("^ = mountain");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("│");
-            Console.WriteLine();
-            Console.Write("│");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write("* = forest  ");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("│");
-            Console.WriteLine();
-            Console.Write("└────────────┘");
-            Console.WriteLine();
         }
         static void enemymovement()
         {
+            if (enemyhp <= 0)
+            {
+                enemypos.Item1 = 0;
+                enemypos.Item2 = 0;
+                return;
+            }
 
 
             Epreviouspos.Item1 = enemypos.Item1;
@@ -172,6 +179,14 @@ namespace TextBasedRPG_MasonSeale
                     enemypos.Item2 = Epreviouspos.Item2;
                 }
             }   
+            if(enemypos.Item1 == otherenemypos.Item1)
+            {
+                if(enemypos.Item2 == otherenemypos.Item2)
+                {
+                    enemypos.Item1 = Epreviouspos.Item1;
+                    enemypos.Item2 = Epreviouspos.Item2;
+                }
+            }
             if (enemystuck == true)
             {
 
@@ -184,22 +199,84 @@ namespace TextBasedRPG_MasonSeale
             {
                 return;
             }
-            Console.SetCursorPosition(enemypos.Item1, enemypos.Item2);
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write("i");
+        }
+        static void otherenemymovement()
+        {
+            if(otherenmyhp <= 0)
+            {
+                otherenemypos.Item1 = 0;
+                otherenemypos.Item2 = 0;
+                return;
+            }
+            if(tired == true)
+            {
+                tired = false;
+                return;
+            }
+            else if (tired == false)
+            {
+                tired = true;
+            }
+            Opreviouspos.Item1 = otherenemypos.Item1;
+            Opreviouspos.Item2 = otherenemypos.Item2;
+            int horv = enemyrng.Next(0, 2);
+            if (horv == 0)
+            {
+                if (playerpos.Item1 > otherenemypos.Item1)
+                {
+                    otherenemypos.Item1 += 1;
+                }
+                if (playerpos.Item1 < otherenemypos.Item1)
+                {
+                    otherenemypos.Item1 -= 1;
+                }
+            }
+            if (horv == 1)
+            {
+                if (playerpos.Item2 > otherenemypos.Item2)
+                {
+                    otherenemypos.Item2 += 1;
+                }
+                if (playerpos.Item2 < otherenemypos.Item2)
+                {
+                    otherenemypos.Item2 -= 1;
+                }
+
+            }
+
+
+            if (otherenemypos.Item1 == playerpos.Item1)
+            {
+                if (otherenemypos.Item2 == playerpos.Item2)
+                {
+                    playerhp -= 1;
+                    otherenemypos.Item1 = Opreviouspos.Item1;
+                    otherenemypos.Item2 = Opreviouspos.Item2;
+                }
+            }
+            if (otherenemypos.Item1 == enemypos.Item1)
+            {
+                if(otherenemypos.Item2 == enemypos.Item2)
+                {
+                    otherenemypos.Item1 = Opreviouspos.Item1;
+                    otherenemypos.Item2 = Opreviouspos.Item2;
+                }
+            }
+            if (enemyhp <= 0)
+            {
+                return;
+            }
+
         }
         static void hud()
         {
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine($"Your HP: {playerhp} ");
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"Enemy HP: {enemyhp} ");
+            Console.WriteLine($"Enemy HP: {enemyhp} Other Enemy HP: {otherenmyhp} ");
         }
         static void playermovment()
         {
-            Console.SetCursorPosition(playerpos.Item1, playerpos.Item2);
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.Write("i");
 
             Ppreviouspos.Item1 = playerpos.Item1;
             Ppreviouspos.Item2 = playerpos.Item2;
@@ -220,6 +297,14 @@ namespace TextBasedRPG_MasonSeale
             {
                 playerpos.Item1 += 1;
             }
+            if (playerstuck == true)
+            {
+
+                playerstuck = false;
+                playerpos.Item1 = Ppreviouspos.Item1;
+                playerpos.Item2 = Ppreviouspos.Item2;
+                return;
+            }
             
             if (playerpos.Item1 == enemypos.Item1)
             {
@@ -231,6 +316,15 @@ namespace TextBasedRPG_MasonSeale
                 }
 
             }
+            if(playerpos.Item1 == otherenemypos.Item1)
+            {
+                if(playerpos.Item2 == otherenemypos.Item2)
+                {
+                    otherenmyhp -= 1;
+                    playerpos.Item1 = Ppreviouspos.Item1;
+                    playerpos.Item2 = Ppreviouspos.Item2;
+                }
+            }
             if (playerpos.Item2 == 3)
             {
                 playerpos.Item2 = Ppreviouspos.Item2;
@@ -239,28 +333,18 @@ namespace TextBasedRPG_MasonSeale
             {
                 playerpos.Item1 = Ppreviouspos.Item1;
             }
-            if (playerpos.Item2 == 16)
+            if (playerpos.Item2 == verticlebounds)
             {
                 playerpos.Item2 = Ppreviouspos.Item2;
             }
-            if(playerpos.Item1 == 32)
+            if (playerpos.Item1 == horziontalbound + 1)
             {
                 playerpos.Item1 = Ppreviouspos.Item1;
             }
-            
-            if (playerstuck == true)
-            {
-
-                playerstuck = false;
-                playerpos.Item1 = Ppreviouspos.Item1;
-                playerpos.Item2 = Ppreviouspos.Item2;
-            }
-            Console.SetCursorPosition(playerpos.Item1, playerpos.Item2);
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.Write("i");
         }
         static void endcheck()
         {
+
             if(playerhp <= 0)
             {
                 going = false;
@@ -268,14 +352,13 @@ namespace TextBasedRPG_MasonSeale
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("You died");
             }
-            if(enemyhp <= 0)
+            if (enemyhp <= 0 && otherenmyhp <= 0)
             {
-                Console.SetCursorPosition(enemypos.Item1, enemypos.Item2);
-                Console.Write("");
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.SetCursorPosition(0, 17);
+                going = false;
+                Console.Clear();
                 Console.WriteLine("You win");
             }
+
         }
         
     }
